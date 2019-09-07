842bbd02c0a4316514e9b5b5f92fdf42a5ddb6c3
Kratharth <kratharthhegde.cs17@bmsce.ac.in>
2019-09-07 12:36:23 +0530

    Sep 7th

---
+++ moisture_servo/moisture_servo.ino
@@ -0,0 +1,29 @@
+#include<Servo.h>
+Servo sm;
+int pos = 0;
+int msPin = A0;
+int msReading = 0;
+
+
+void setup(){
+  sm.attach(9);
+  Serial.begin(9600);
+}
+
+void loop(){
+  msReading = analogRead(msPin);
+  Serial.println(msReading);
+  boolean att = sm.attached(9);
+  Serial.println(att);
+  if(msReading > 800){
+    for(int i=0; i<=180; i++){
+      sm.write(i);
+      delay(15);
+    }
+    for(pos =180; pos >=0; pos -=1){
+      sm.write(pos);
+      delay(15);
+    }
+  }
+  delay(1000);
+}
